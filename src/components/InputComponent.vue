ba4fb5fb4d981ea652746c633aae9ff45a9da51a
Bill1907 <a31829660@gamil.com>
2022-06-14 17:02:25 +0900

    input component 추가

---
+++ src/components/InputComponent.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <label for="uri">URI
+      <input type="text" id="uri" v-model="uri">
+    </label>
+    <button @click="handleClickButton">Search</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { mapActions, mapState } from 'pinia';
+import {
+  defineComponent, Ref, ref, UnwrapRef,
+} from 'vue';
+import { useUrlDataStore } from '../store/urlData';
+
+export default defineComponent({
+  name: 'InputComponent',
+  computed: {
+    ...mapState(useUrlDataStore, [
+      'uri',
+    ]),
+  },
+  methods: {
+    ...mapActions(useUrlDataStore, [
+      'setUri',
+    ]),
+    handleClickButton() {
+      this.setUri(this.uri);
+    },
+  },
+  setup: () => {
+    const uri: Ref<UnwrapRef<string>> = ref('');
+    return { uri };
+  },
+});
+</script>
+
+<style scoped lang="scss">
+</style>
