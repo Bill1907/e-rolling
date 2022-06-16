ed0532a294a08124324df812b3b9bd08323cda3c
Bill1907 <a31829660@gamil.com>
2022-06-16 15:26:03 +0900

    크롤링 해서 받아온 이미지 자원을 리스트에 추가 기능

--- src/modules/crawling/crawling.ts
+++ src/modules/crawling/crawling.ts
@@ -1,11 +1,26 @@
-import axios from 'axios';
-import cheerio from 'cheerio';
+import puppeteer from 'puppeteer';
+import * as cheerio from 'cheerio';
 
-export const getHTML = async (url: string): Promise<any> => {
+export const getHTML = async (url: string): Promise<string[] | string> => {
   try {
-    return await axios.get(url);
+    const arr: Array<string> = [];
+    const browser = await puppeteer.launch({
+      headless: true,
+      args: [
+        '--disable-web-security',
+      ],
+    });
+    const page = await browser.newPage();
+    await page.goto(url, { waitUntil: 'domcontentloaded' });
+    const bodyHandle = await page.$('body');
+    const html = await page.evaluate((body) => body.innerHTML, bodyHandle);
+    const $ = cheerio.load(html);
+    const list = $('img');
+    list.map((el: number) => arr.push(list[el].attribs.src));
+    return arr;
   } catch (err) {
-    console.log(err);
-    return err;
+    let message = 'UnKnown Error';
+    if (err instanceof Error) message = err.message;
+    return message;
   }
 };
