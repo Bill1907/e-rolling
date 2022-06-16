d5e0dbf12dce4f9026341fcbc34fdbdbd9285cfc
Bill1907 <a31829660@gamil.com>
2022-06-16 15:26:37 +0900

    이미지리스트 리스트 추가, 리스트에 추가하는 기능 추가

--- src/store/urlData.ts
+++ src/store/urlData.ts
@@ -2,12 +2,16 @@ import { defineStore } from 'pinia';
 
 export const useUrlDataStore = defineStore('urlData', {
   state: () => ({
-    uri: '' as string,
+    uri: 'https://unsplash.com/' as string,
+    imageList: [] as string[],
   }),
   getters: {},
   actions: {
     setUri(data: string) {
       this.uri = data;
+    },
+    addImageList(data: string) {
+      this.imageList.push(data);
     },
   },
 });
